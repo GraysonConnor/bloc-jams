c68c538e450d6573f71cf9a50289b24d4deed7c6
Grayson Connor <grayson.bolon@yahoo.com>
2017-09-28 14:34:30 -0500

    checkpoint 9 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,28 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-        var points = document.getElementsByClassName('point');
-
-        var revealPoint = function(x) {
-          points[x].style.opacity = 1;
-          points[x].style.transform = "scaleX(1) translateY(0)";
-          points[x].style.msTransform = "scaleX(1) translateY(0)";
-          points[x].style.WebkitTransform = "scaleX(1) translateY(0)";
-        };
-
-        for(var i=0; i<points.length; i++) {
-          revealPoint(i);
-        }
+var revealPoint = function(point) {
+  points.style.opacity = 1;
+  points.style.transform = "scaleX(1) translateY(0)";
+  points.style.msTransform = "scaleX(1) translateY(0)";
+  points.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
 
+ var animatePoints = function(points) {
+        forEach(points, revealPoint);
     };
+
+    window.onload = function() {
+      if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+      var sellingPoints = document.getElementsByClassName('selling-points')[0];
+       var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+      window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+         }
+
+
+      });
+ }
